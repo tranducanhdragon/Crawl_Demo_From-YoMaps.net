d681fcd3e41b681c962b5a4ec29eedb06f3a3c4d
tranducanhvip123_gmail.com <tranducanhvip123@gmail.com>
2021-11-30 17:27:18 +0700

    init

---
+++ server.js
@@ -0,0 +1,52 @@
+const cheerio = require('cheerio');
+const request = require('request-promise');
+const fs = require('fs');
+
+async function getAll(){
+  let data = [];
+  await request('https://yomaps.net/vi/VN/24-ha-noi/type/33-electronics-store/', (error, response, html) => {
+      if(!error && response.statusCode == 200) {
+          let $ = cheerio.load(html);
+          // data = $('.block-grid-v2-info').find('.text-ellipsis a').attr('href');
+          $('.block-grid-v2-info').each((index, el) => {
+            data.push(el.children[1].children[0].attribs.href);
+          })
+      }
+      else {
+      console.log(error);
+      }
+  });
+  return data;
+}
+async function getDetail(url){
+  let result = {};
+  await request('https://yomaps.net'+url.toString(), (error, response, htmlDetail) => {
+    if(!error && response.statusCode == 200){
+      let $ = cheerio.load(htmlDetail);
+      let type = $('tr:eq(0)').find('td:eq(1)').text();
+      let store = $('h1').text();
+      let address = $('tr:eq(1)').find('td:eq(1)').text();
+      let location = $('tr:eq(2)').find('td:eq(1)').find('a').text();
+      let phone = $('tr:eq(3)').find('td:eq(1)').text();
+      let site = $('tr:eq(4)').find('td:eq(1)').text();
+      result = {type, store, address, location, phone, site};
+    }
+  })
+  return result;
+}
+function main(){
+  let data = [],
+  result = [];
+
+  data = getAll();
+  data.then((res) => {
+    debugger
+    res.forEach(item => {
+      getDetail(item).then((res) => {
+        result.push(res);
+        fs.writeFileSync('electric-store.json', JSON.stringify(result)); // lưu dữ liệu vào file data.json
+      });
+    })
+  })
+}
+main()
