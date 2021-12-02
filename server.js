04f43c0d376076a0173c3c83166b0b9f0199d88d
tranducanhvip123_gmail.com <tranducanhvip123@gmail.com>
2021-12-02 09:37:09 +0700

    update_02.12.21

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ async function getAll(){
   let data = [],
       page = 1;
   for(page = 1; page <= 11; ++page){
-    await request('https://yomaps.net/vi/VN/24-ha-noi/type/61-moving-company?page='+page.toString()+'/', (error, response, html) => {
+    await request('https://yomaps.net/vi/VN/24-ha-noi/type/56-meal-delivery?page='+page.toString()+'/', (error, response, html) => {
         if(!error && response.statusCode == 200) {
             let $ = cheerio.load(html);
             // data = $('.block-grid-v2-info').find('.text-ellipsis a').attr('href');
@@ -32,7 +32,13 @@ async function getDetail(url){
       let location = $('tr:eq(2)').find('td:eq(1)').find('a').text();
       let phone = $('tr:eq(3)').find('td:eq(1)').text();
       let site = $('tr:eq(4)').find('td:eq(1)').text();
-      result = {type, store, address, location, phone, site};
+      let img = [];
+      $('.fancybox > img').each((i, it) => {
+        if(it && it.attribs && it.attribs.src){
+          img.push(it.attribs.src);
+        }
+      });
+      result = {type, store, address, location, phone, site, img};
     }
   })
   return result;
@@ -46,7 +52,7 @@ function main(){
     res.forEach(item => {
       getDetail(item).then((res) => {
         result.push(res);
-        fs.writeFileSync('logistic-store.json', JSON.stringify(result)); // lưu dữ liệu vào file data.json
+        fs.writeFileSync('food-store.json', JSON.stringify(result)); // lưu dữ liệu vào file data.json
       });
     })
   })
